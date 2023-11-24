342355f554dfd34e269ffb4c7631a593b69ae949
janouskie <b00149565@mytudublin.ie>
2023-11-24 18:10:19 +0000

    with functional buttons!
    hopefully

---
+++ Challenge 5/Assets/Challenge 5/Scripts/Link.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+    public void OpenGithub()
+    {
+        Application.OpenURL("https://github.com/janouskie/Challenge5");
+    }
+}
